f02a643fc125d4d2b5685bdac9d691650be0e4ec
adrian <adrian@programa.com>
2024-09-19 10:23:47 +0200

    He añadido una tercera linea en la que se muestra mi comida favorita

--- prueba.java
+++ prueba.java
@@ -1,6 +1,7 @@
 public class prueba {
 	public static void main(String args[]){
 		System.out.println("Suarez Macias");
-		System.out.println("19 años - 2005");		
+		System.out.println("19 años - 2005");
+		System.out.println("Pasta");		
 	}
 }
