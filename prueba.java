cfca0e33ab7ea76fdf4ac80444e5eb140af8f590
adrian <adrian@programa.com>
2024-09-19 10:18:54 +0200

    He añadido una segunda linea que dice mi edad y año de nacimiento

--- prueba.java
+++ prueba.java
@@ -1,5 +1,6 @@
 public class prueba {
 	public static void main(String args[]){
-		System.out.println("Suarez Macias");	
+		System.out.println("Suarez Macias");
+		System.out.println("19 años - 2005");		
 	}
 }
