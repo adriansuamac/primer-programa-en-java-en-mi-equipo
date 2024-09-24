2f6252a75ac007c4f96afef49044cf546b796c15
adrian <adrian@programa.com>
2024-09-24 09:34:23 +0200

    Ahora he añadido mi cantante favorito

--- prueba.java
+++ prueba.java
@@ -1,7 +1,7 @@
 public class prueba {
 	public static void main(String args[]){
-		System.out.println("Suarez Macias");
 		System.out.println("19 años - 2005");
-		System.out.println("Pasta");		
+		System.out.println("Pasta");	
+		System.out.println("Melendi");	
 	}
 }
